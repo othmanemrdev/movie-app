b0807fd88f82ea10162c5a0752d031cb3d19be5f
Othmane Hilal <othmanehilal200255@gmail.com>
2023-04-28 11:53:26 +0100

    Search bar added

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bienvenue sur la page d'accueil</Text>
+      <TextInput
+    placeholder="Rechercher..."
+    style={styles.searchBar}
+  />
+      <Text style={styles.text}>page d'accueil</Text>
     </View>
   );
 }
@@ -19,4 +23,15 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
+  searchBar: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    width: '90%',
+  },
 });
+
